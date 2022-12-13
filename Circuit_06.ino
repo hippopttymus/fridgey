373aeab80efe69d5f1a72429cf9e002e5ab16076
hippopttymus <45108329+hippopttymus@users.noreply.github.com>
2022-12-12 23:25:02 -0600

    Add files via upload

---
+++ Circuit_06.ino
@@ -0,0 +1,228 @@
+/*
+
+Example sketch 06
+
+PHOTO RESISTOR
+
+  Use a photoresistor (light sensor) to control the brightness
+  of a LED.
+
+Hardware connections:
+
+  Photo resistor:
+  
+    Connect one side of the photoresistor to 5 Volts (5V).
+    Connect the other side of the photoresistor to ANALOG pin 0.
+    Connect a 10K resistor between ANALOG pin 0 and GND.
+
+    This creates a voltage divider, with the photoresistor one
+    of the two resistors. The output of the voltage divider
+    (connected to A0) will vary with the light level.
+    
+  LED:
+  
+    Connect the positive side (long leg) of the LED to
+    digital pin 9. (To vary the brightness, this pin must
+    support PWM, which is indicated by "~" or "PWM" on the
+    Arduino itself.)
+
+    Connect the negative side of the LED (short leg) to a
+    330 Ohm resistor.
+
+    Connect the other side of the resistor to GND.
+
+This sketch was written by SparkFun Electronics,
+with lots of help from the Arduino community.
+This code is completely free for any use.
+Visit http://www.arduino.cc to learn about the Arduino.
+
+Version 2.0 6/2012 MDG
+*/
+
+
+// As usual, we'll create constants to name the pins we're using.
+// This will make it easier to follow the code below.
+
+//const int sensorPin = 0;
+const int servoMotorPWM = 9;
+
+// We'll also set up some global variables for the light level:
+
+int lightLevel, high = 0, low = 1023;
+const int buzzerPin = 10;
+#include <Servo.h>
+Servo servo1;
+
+const int temperaturePin = 0;
+const int sensorPin = 2;
+void setup()
+{
+  // We'll set up the LED pin to be an output.
+  // (We don't need to do anything special to use the analog input.)
+  
+  pinMode(servoMotorPWM, OUTPUT);
+  Serial.begin(9600);
+  servo1.attach(9);
+
+  pinMode(buzzerPin, OUTPUT);
+
+}
+
+int position;
+
+void loop()
+{
+
+  float voltage, degreesC, degreesF, lightV;
+  // Just as we've done in the past, we'll use the analogRead()
+  // function to measure the voltage coming from the photoresistor
+  // resistor pair. This number can range between 0 (0 Volts) and
+  // 1023 (5 Volts), but this circuit will have a smaller range
+  // between dark and light.
+  voltage = getVoltage();
+
+  
+  
+  degreesC = (voltage - 0.5) * 100.0;
+  degreesF = degreesC * (9.0/5.0) + 32.0;
+  Serial.println(degreesF);
+
+  if(degreesF > 90 && degreesF < 150){
+    alarm(2);
+  }
+
+
+  lightLevel = analogRead(A0);
+
+  Serial.print(lightLevel);
+  Serial.println("  ");
+  // check light and sound alarm if light is on
+  if(lightLevel < 600){
+    delay(10000);
+    for(int i = 0; i < 5; i++){
+        alarm(1); 
+    }
+    servo1.write(180);
+    delay(5000); 
+    servo1.write(60);
+    delay(5000); 
+  }
+}
+
+void alarm(int i){
+  if(i >1){
+    tone(buzzerPin, 262, 500);
+  }else{
+    tone(buzzerPin, 523, 5000);
+  }
+}
+  
+float getVoltage(){
+
+  return (analogRead(A2) * 0.004882814);
+}
+
+void manualTune()
+{
+  // As we mentioned above, the light-sensing circuit we built
+  // won't have a range all the way from 0 to 1023. It will likely
+  // be more like 300 (dark) to 800 (light). If you run this sketch
+  // as-is, the LED won't fully turn off, even in the dark.
+  
+  // You can accommodate the reduced range by manually 
+  // tweaking the "from" range numbers in the map() function.
+  // Here we're using the full range of 0 to 1023.
+  // Try manually changing this to a smaller range (300 to 800
+  // is a good guess), and try it out again. If the LED doesn't
+  // go completely out, make the low number larger. If the LED
+  // is always too bright, make the high number smaller.
+
+  // Remember you're JUST changing the 0, 1023 in the line below!
+
+  lightLevel = map(lightLevel, 0, 1023, 0, 255);
+  lightLevel = constrain(lightLevel, 0, 255);
+
+  // Now we'll return to the main loop(), and send lightLevel
+  // to the LED.
+} 
+
+
+void autoTune()
+{
+  // As we mentioned above, the light-sensing circuit we built
+  // won't have a range all the way from 0 to 1023. It will likely
+  // be more like 300 (dark) to 800 (light).
+  
+  // In the manualTune() function above, you need to repeatedly
+  // change the values and try the program again until it works.
+  // But why should you have to do that work? You've got a
+  // computer in your hands that can figure things out for itself!
+
+  // In this function, the Arduino will keep track of the highest
+  // and lowest values that we're reading from analogRead().
+
+  // If you look at the top of the sketch, you'll see that we've
+  // initialized "low" to be 1023. We'll save anything we read
+  // that's lower than that:
+  
+  if (lightLevel < low)
+  {
+    low = lightLevel;
+  }
+
+  // We also initialized "high" to be 0. We'll save anything
+  // we read that's higher than that:
+  
+  if (lightLevel > high)
+  {
+    high = lightLevel;
+  }
+  
+  // Once we have the highest and lowest values, we can stick them
+  // directly into the map() function. No manual tweaking needed!
+  
+  // One trick we'll do is to add a small offset to low and high,
+  // to ensure that the LED is fully-off and fully-on at the limits
+  // (otherwise it might flicker a little bit).
+  
+  lightLevel = map(lightLevel, low+30, high-30, 0, 255);
+  lightLevel = constrain(lightLevel, 0, 255);
+  
+  // Now we'll return to the main loop(), and send lightLevel
+  // to the LED.
+}
+
+int frequency(char note) 
+{
+  // This function takes a note character (a-g), and returns the
+  // corresponding frequency in Hz for the tone() function.
+  
+  int i;
+  const int numNotes = 8;  // number of notes we're storing
+  
+  // The following arrays hold the note characters and their
+  // corresponding frequencies. The last "C" note is uppercase
+  // to separate it from the first lowercase "c". If you want to
+  // add more notes, you'll need to use unique characters.
+
+  // For the "char" (character) type, we put single characters
+  // in single quotes.
+
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  
+  // Now we'll search through the letters in the array, and if
+  // we find it, we'll return the frequency for that note.
+  
+  for (i = 0; i < numNotes; i++)  // Step through the notes
+  {
+    if (names[i] == note)         // Is this the one?
+    {
+      return(frequencies[i]);     // Yes! Return the frequency
+    }
+  }
+  return(0);  // We looked through everything and didn't find it,
+              // but we still need to return a value, so return 0.
+}
+
+
